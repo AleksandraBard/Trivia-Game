9baff03665f9f4ff750e64e28775a6537372e4f7
Aleksandra Bardymova <bardymovasasha@gmail.com>
2020-10-28 09:42:19 -0400

    starting point

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+export default class App extends React.Component {
+    constructor() {
+        super()
+
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Hello World!</p>
+            </div>
+        )
+    }
+}
+
